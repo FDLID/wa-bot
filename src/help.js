c6fdcda699717e0d5d1ddd7b9b4d310b561f27d3
FDL <77911760+FDLBOT@users.noreply.github.com>
2021-04-11 20:52:54 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,48 @@
+const help = (prefix) => {
+	return `
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+ *WHATSAPP BOT*
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+
+╔════════════════════
+║ *STICKER MENU*
+╠════════════════════
+╠◪ *${prefix}sticker*
+╠◪ *${prefix}sticker nobg*
+╠◪ *${prefix}toimg*
+╠◪ *${prefix}tsticker
+╠════════════════════
+║ *MEME MENU*
+╠════════════════════
+╠◪ *${prefix}meme*
+╠════════════════════
+║ *OTHERS MENU*
+╠════════════════════
+╠◪ *${prefix}tp*
+╠◪ *${prefix}ep*
+╠◪ *${prefix}tahta*
+╠◪ *${prefix}gtts*
+╠◪ *${prefix}loli*
+╠◪ *${prefix}nsfwloli*
+╠◪ *${prefix}url2img*
+╠◪ *${prefix}simi*
+╠◪ *${prefix}ocr*
+╠◪ *${prefix}wait*
+╠◪ *${prefix}setprefix*
+╠════════════════════
+║ *GROUP MENU*
+╠════════════════════
+╠◪ *${prefix}add*
+╠◪ *${prefix}kick*
+╠◪ *${prefix}promote*
+╠◪ *${prefix}demote*
+╠◪ *${prefix}linkgroup*
+╠◪ *${prefix}leave*
+╠◪ *${prefix}tagall*
+╠◪ *${prefix}simih*
+╠════════════════════
+║ *BY FDL & RR-018*
+╚════════════════════`
+}
+
+exports.help = help
